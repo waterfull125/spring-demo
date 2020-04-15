ed41d912709bfda024819e6ce8acc3c63c309c7f
waterfull125 <499093895@qq.com>
2020-04-15 23:08:07 +0800

    集成mybatis plus

--- src/main/java/com/cgx/springdemo/SpringDemoApplication.java
+++ src/main/java/com/cgx/springdemo/SpringDemoApplication.java
@@ -1,9 +1,11 @@
 package com.cgx.springdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com/cgx/springdemo/mapper")
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {

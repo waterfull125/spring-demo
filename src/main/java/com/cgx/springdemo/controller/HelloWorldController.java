4b079500da4a6daac126eece18b23289f4649a15
49909 <499093895@qq.com>
2020-05-08 18:43:55 +0800

    TAddress接口保存传入参数

--- src/main/java/com/cgx/springdemo/controller/HelloWorldController.java
+++ src/main/java/com/cgx/springdemo/controller/HelloWorldController.java
@@ -2,16 +2,22 @@ package com.cgx.springdemo.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.cgx.springdemo.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("")
 public class HelloWorldController {
+    @Autowired
+    HelloWorldService helleService;
+
     @RequestMapping(value = "/json/data",method = RequestMethod.POST,produces = "application/json;charset=utf-8;")
     @ResponseBody
     public String hello(@RequestBody String para){
         JSONObject json = JSON.parseObject(para);
         System.out.println(json.toString());
-        return json.toString();
+        String resultOfservice =helleService.hello();
+        return json.toString()+resultOfservice;
     }
 }
